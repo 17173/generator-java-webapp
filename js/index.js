f8ac831429c3e961e1d6ab527e4ca556defd37b1
lynzz <lynzz168@gmail.com>
2014-04-16 15:32:33 +0800

    添加子生产器 js

---
+++ js/index.js
@@ -0,0 +1,17 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+
+var JsGenerator = module.exports = function JsGenerator(args, options, config) {
+  // By calling `NamedBase` here, we get the argument to the subgenerator call
+  // as `this.name`.
+  yeoman.generators.NamedBase.apply(this, arguments);
+};
+
+util.inherits(JsGenerator, yeoman.generators.NamedBase);
+
+JsGenerator.prototype.files = function files() {
+  this.jsRoot = '${jsRoot}';
+  this.template('app.js', path.join('src/app/', this.name + '/index.js'));
+};
